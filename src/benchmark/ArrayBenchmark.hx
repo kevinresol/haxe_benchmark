9418434f1d2d6db03f8e5c00a1808d5877f249c2
Kevin Leung <kevinresol@gmail.com>
2018-09-26 15:29:41 +0800

    More

--- src/benchmark/ArrayBenchmark.hx
+++ src/benchmark/ArrayBenchmark.hx
@@ -11,6 +11,7 @@ class ArrayBenchmark {
 	@:variant(0.1)
 	@:variant('0')
 	@:variant(true)
+	@:variant(this)
 	public function push<T>(v:T):Assertions {
 		var array = [];
 		return benchmark(1000000, array.push(v));
@@ -21,11 +22,58 @@ class ArrayBenchmark {
 	@:variant(0.1)
 	@:variant('0')
 	@:variant(true)
+	@:variant(this)
 	public function iterate<T>(v:T):Assertions {
 		var array = [for(_ in 0...1000000) v];
 		return benchmark(100, {
 			var count = 0;
 			for(i in array) count++;
+		});
+	}
+	
+	@:generic
+	@:variant(0)
+	@:variant(0.1)
+	@:variant('0')
+	@:variant(true)
+	@:variant(this)
+	public function getFirst<T>(v:T):Assertions {
+		var array = [for(_ in 0...1000000) v];
+		return benchmark(100, array[0]);
+	}
+	
+	@:generic
+	@:variant(0)
+	@:variant(0.1)
+	@:variant('0')
+	@:variant(true)
+	@:variant(this)
+	public function getLast<T>(v:T):Assertions {
+		var array = [for(_ in 0...1000000) v];
+		return benchmark(1000000, array[1000000 - 1]);
+	}
+	
+	@:generic
+	@:variant(0)
+	@:variant(0.1)
+	@:variant('0')
+	@:variant(true)
+	@:variant(this)
+	public function copy<T>(v:T):Assertions {
+		var array = [for(_ in 0...1000000) v];
+		return benchmark(100, array.copy());
+	}
+	
+	@:generic
+	@:variant(0)
+	@:variant(0.1)
+	@:variant('0')
+	@:variant(true)
+	@:variant(this)
+	public function splice<T>(v:T):Assertions {
+		var array = [for(_ in 0...100000) v];
+		return benchmark(1, {
+			for(i in 0...array.length) array.splice(i, 1);
 		});
 	}
 }
