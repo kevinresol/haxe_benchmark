8281e74d3fe0717aa05c0ec66fd28ba27f2da95e
Kevin Leung <kevinresol@gmail.com>
2018-09-27 12:14:16 +0800

    More

--- src/benchmark/ArrayBenchmark.hx
+++ src/benchmark/ArrayBenchmark.hx
@@ -4,6 +4,8 @@ import tink.unit.Assert.*;
 import tink.testrunner.*;
 
 class ArrayBenchmark {
+	var result:Int = 0;
+	
 	public function new() {}
 	
 	@:generic
@@ -26,8 +28,8 @@ class ArrayBenchmark {
 	public function iterate<T>(v:T):Assertions {
 		var array = [for(_ in 0...1000000) v];
 		return benchmark(100, {
-			var count = 0;
-			for(i in array) count++;
+			result = 0;
+			for(i in array) result++;
 		});
 	}
 	
@@ -39,7 +41,8 @@ class ArrayBenchmark {
 	@:variant(this)
 	public function getFirst<T>(v:T):Assertions {
 		var array = [for(_ in 0...1000000) v];
-		return benchmark(1000000, array[0]);
+		var temp;
+		return benchmark(1000000, temp = array[0]);
 	}
 	
 	@:generic
@@ -50,7 +53,8 @@ class ArrayBenchmark {
 	@:variant(this)
 	public function getLast<T>(v:T):Assertions {
 		var array = [for(_ in 0...1000000) v];
-		return benchmark(1000000, array[1000000 - 1]);
+		var temp;
+		return benchmark(1000000, temp = array[1000000 - 1]);
 	}
 	
 	@:generic
@@ -70,10 +74,45 @@ class ArrayBenchmark {
 	@:variant('0')
 	@:variant(true)
 	@:variant(this)
-	public function splice<T>(v:T):Assertions {
+	public function spliceHead<T>(v:T):Assertions {
 		var array = [for(_ in 0...100000) v];
-		return benchmark(1, {
-			for(i in 0...array.length) array.splice(i, 1);
-		});
+		return benchmark(100000, array.splice(0, 1));
+	}
+	
+	@:generic
+	@:variant(0)
+	@:variant(0.1)
+	@:variant('0')
+	@:variant(true)
+	@:variant(this)
+	public function spliceTail<T>(v:T):Assertions {
+		var array = [for(_ in 0...100000) v];
+		return benchmark(100000, array.splice(array.length - 1, 1));
+	}
+	
+	@:generic
+	@:variant(0, 1)
+	@:variant(0.1, 0.2)
+	@:variant('0', '1')
+	@:variant(true, false)
+	@:variant(this, new benchmark.ArrayBenchmark())
+	public function indexOfHead<T>(v:T, i:T):Assertions {
+		var array = [for(_ in 0...1000) v];
+		array[0] = i;
+		result = 0;
+		return benchmark(1000, result += array.indexOf(i));
+	}
+	
+	@:generic
+	@:variant(0, 1)
+	@:variant(0.1, 0.2)
+	@:variant('0', '1')
+	@:variant(true, false)
+	@:variant(this, new benchmark.ArrayBenchmark())
+	public function indexOfTail<T>(v:T, i:T):Assertions {
+		var array = [for(_ in 0...1000) v];
+		array[array.length - 1] = i;
+		result = 0;
+		return benchmark(1000, result += array.indexOf(i));
 	}
 }
