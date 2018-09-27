487b82f90b36a6a4d04a8eebade88b4642c2fa73
Kevin Leung <kevinresol@gmail.com>
2018-09-27 20:51:10 +0800

    Adjust a bit the number of iterations per target

--- src/benchmark/ArrayBenchmark.hx
+++ src/benchmark/ArrayBenchmark.hx
@@ -26,8 +26,8 @@ class ArrayBenchmark {
 	@:variant(true)
 	@:variant(this)
 	public function iterate<T>(v:T):Assertions {
-		var array = [for(_ in 0...1000000) v];
-		return benchmark(100, {
+		var array = [for(_ in 0...10000) v];
+		return benchmark(#if (php || python || lua) 100 #else 10000 #end, {
 			result = 0;
 			for(i in array) result++;
 		});
@@ -65,7 +65,7 @@ class ArrayBenchmark {
 	@:variant(this)
 	public function copy<T>(v:T):Assertions {
 		var array = [for(_ in 0...1000000) v];
-		return benchmark(100, array.copy());
+		return benchmark(#if php 10000 #elseif lua 1 #else 100 #end, array.copy());
 	}
 	
 	@:generic
@@ -76,7 +76,7 @@ class ArrayBenchmark {
 	@:variant(this)
 	public function spliceHead<T>(v:T):Assertions {
 		var array = [for(_ in 0...100000) v];
-		return benchmark(100000, array.splice(0, 1));
+		return benchmark(#if (php || lua) 100 #elseif js 10000 #else 100000 #end, array.splice(0, 1));
 	}
 	
 	@:generic
@@ -87,7 +87,7 @@ class ArrayBenchmark {
 	@:variant(this)
 	public function spliceTail<T>(v:T):Assertions {
 		var array = [for(_ in 0...100000) v];
-		return benchmark(100000, array.splice(array.length - 1, 1));
+		return benchmark(#if php 100 #else 100000 #end, array.splice(array.length - 1, 1));
 	}
 	
 	@:generic
@@ -100,7 +100,7 @@ class ArrayBenchmark {
 		var array = [for(_ in 0...1000) v];
 		array[0] = i;
 		result = 0;
-		return benchmark(1000, result += array.indexOf(i));
+		return benchmark(#if (php || js || cpp) 100000 #else 1000 #end, result += array.indexOf(i));
 	}
 	
 	@:generic
@@ -113,6 +113,9 @@ class ArrayBenchmark {
 		var array = [for(_ in 0...1000) v];
 		array[array.length - 1] = i;
 		result = 0;
-		return benchmark(1000, result += array.indexOf(i));
+		return benchmark(#if (js || cpp) 100000 #else 1000 #end, result += array.indexOf(i));
 	}
+	
+	@:keep
+	function dummy<T>(v:T) {}
 }
