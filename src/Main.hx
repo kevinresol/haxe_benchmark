9f82917d06d92b513e578b2e396e8e8f7590fba0
Kevin Leung <kevinresol@gmail.com>
2018-09-27 19:03:09 +0800

    Add anchor

--- src/Main.hx
+++ src/Main.hx
@@ -1,13 +1,22 @@
 package;
 
+import travix.*;
 import benchmark.*;
 import tink.unit.*;
 import tink.testrunner.*;
 
 class Main {
+	
+	static inline var START = '>> Haxe Benchmark Log Start <<';
+	static inline var END = '>> Haxe Benchmark Log End <<';
+	
 	static function main() {
+		Logger.println(START);
 		Runner.run(TestBatch.make([
 			new ArrayBenchmark(),
-		])).handle(Runner.exit);
+		])).handle(function(result) {
+			Logger.println(END);
+			Runner.exit(result);
+		});
 	}
 }
